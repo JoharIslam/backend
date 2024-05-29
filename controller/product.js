a86b7364b0e62fc7db01722462ecc69ec162fa8f
JoharIslam <91312613+JoharIslam@users.noreply.github.com>
2024-05-29 21:22:17 +0500

    init

--- controller/product.js
+++ controller/product.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Product = require("../model/product");
 const Order = require("../model/order");
 const Shop = require("../model/shop");
-const cloudinary = require("cloudinary");x
+const cloudinary = require("cloudinary");
 const ErrorHandler = require("../utils/ErrorHandler");
 
 // create product
